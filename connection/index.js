965140877d0dc17fd4ff382c1e26634c4d1ef73a
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2023-01-23 10:27:44 -0300

    main

---
+++ connection/index.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var bodyParse = require('body-parse');
+var W3CWebSocket = require('websocket').w3cwebsocket;
+
+console.log("Hello World");
+
+
+var client = new W3CWebSocket('ws:// http://host.docker.internal:4330/GetBillBoard');
+
+client.onerror = function() {
+    console.log('Connection Error');
+};
+
+client.onopen = function() {
+    console.log('WebSocket Client Connected');
+
+    function sendNumber() {
+        if (client.readyState === client.OPEN) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            client.send(number.toString());
+            setTimeout(sendNumber, 1000);
+        }
+    }
+    sendNumber();
+};
+
+client.onclose = function() {
+    console.log('echo-protocol Client Closed');
+};
+
+client.onmessage = function(e) {
+    if (typeof e.data === 'string') {
+        console.log("Received: '" + e.data + "'");
+    }
+};
